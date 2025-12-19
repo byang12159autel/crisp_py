93d4bba92c5c7a2afe49453f73e8680f1905468c
Benjamin Yang <benjaminyang@autel.com>
2025-12-18 19:11:43 -0500

    shorten stowaway time

--- examples/waypoint_navigation.py
+++ examples/waypoint_navigation.py
@@ -222,13 +222,13 @@ robot.joint_trajectory_controller_client.send_joint_config(
 )
 # Wait for robot to be ready again
 robot.wait_until_ready()
-time.sleep(1.0)
+time.sleep(3.0)
 
 # Send the joint configuration command
 robot.joint_trajectory_controller_client.send_joint_config(
     joint_names=robot.config.joint_names,
     joint_config=nodes.get("stowaway_transition_joint_config"),
-    time_to_goal=5.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
+    time_to_goal=3.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
     blocking=True         # Wait until motion completes
 )
 # Wait for robot to be ready again
@@ -239,7 +239,7 @@ time.sleep(0.5)
 robot.joint_trajectory_controller_client.send_joint_config(
     joint_names=robot.config.joint_names,
     joint_config=nodes.get("Home_joint_config"),
-    time_to_goal=5.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
+    time_to_goal=3.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
     blocking=True         # Wait until motion completes
 )
 # Wait for robot to be ready again
@@ -358,7 +358,7 @@ time.sleep(1.0)
 robot.joint_trajectory_controller_client.send_joint_config(
     joint_names=robot.config.joint_names,
     joint_config=nodes.get("stowaway_transition_joint_config"),
-    time_to_goal=5.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
+    time_to_goal=3.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
     blocking=True         # Wait until motion completes
 )
 # Wait for robot to be ready again
@@ -369,7 +369,7 @@ time.sleep(0.5)
 robot.joint_trajectory_controller_client.send_joint_config(
     joint_names=robot.config.joint_names,
     joint_config=nodes.get("stowaway_joint_config"),
-    time_to_goal=5.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
+    time_to_goal=3.0,  # 5 seconds to reach home (same as robot.config.time_to_home)
     blocking=True         # Wait until motion completes
 )
 # Wait for robot to be ready again
